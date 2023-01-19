de6ee44cbd2f1f0e9cc601b7cda7f905f1bd6762
pashadecoded <99236275+pashadecoded@users.noreply.github.com>
2023-01-19 17:46:43 +0530

    Update bash-k8s-logs-scripts.sh

--- bash-k8s-logs-scripts.sh
+++ bash-k8s-logs-scripts.sh
@@ -9,7 +9,7 @@ get_logs(){
         # Get current date
         today=Logs@$(date +"%F#%T")
         # Get the namespace name
-        namespace=<Namespace>
+        namespace=<Name-Space>
         # Create a directory to store the logs with the current date
         mkdir -p $today
         # Get a list of all deployments in the specified namespace
@@ -20,17 +20,39 @@ get_logs(){
         echo -e "${green}\nCollecting Logs from all the available pods."
         echo -e "\nFrom Namespace: $namespace"
         echo -e "\nPlease wait.\n${reset}"
-        echo -e "${red}#############################################${reset}"
+        #echo -e "${red}#############################################${reset}"
         for pod in $pods; do
         # Get the logs for the deployment and save to a file
         kubectl logs $pod --all-containers=true -n $namespace > $today/$pod.log 2>>$today/err.log
         done
 }
 
+install_zip(){
+
+        if dpkg-query -W -f='${Status}' zip 2>/dev/null | grep -q "ok installed"; then
+        # echo "zip is already installed"
+        :
+        else
+        # Install zip
+                sudo apt-get update >/dev/null 2>&1
+                sudo apt-get install -y zip >/dev/null 2>&1
+        # echo "zip has been installed"
+        fi
+
+}
+
 log_zip(){
-        sudo apt-get install zip -y 2>&1>/dev/null
+
+        local blue=`tput setaf 4`
+        local green=`tput setaf 2`
+        local red=`tput setaf 1`
+        local magenta=`tput setaf 5`
+        local reset=`tput sgr0`
         zip -r -1 -9 -q $today.zip $today
+        echo -e "${green}Done.\n${reset}"
+        echo -e "${red}#############################################${reset}"
 }
 
 get_logs
+install_zip
 log_zip
